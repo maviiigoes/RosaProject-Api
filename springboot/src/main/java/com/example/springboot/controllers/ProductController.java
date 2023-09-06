e8298bb65c63b1a04b7e5154462bddc040af1e2d
Maria Vitória S.G <110963488+maviiigoes@users.noreply.github.com>
2023-09-05 21:26:05 -0300

    Update

--- springboot/src/main/java/com/example/springboot/controllers/ProductController.java
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.springboot.dtos.ProductRecordDto;
@@ -48,7 +49,7 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.OK).body(productO.get());
         }
 
-     @GetMapping("/products/{id}")
+     @PutMapping("/products/{id}")
         public ResponseEntity <Object> updateProduct(@PathVariable(value= "id") UUID id,
                                                         @RequestBody @Valid ProductRecordDto productRecordDto){
             Optional <ProductModel> productO = productRepository.findById(id);
